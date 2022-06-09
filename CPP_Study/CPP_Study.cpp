f3dbe0c0ebb996d4b4c406642fae6edb3260dcb6
cheonyongjun <yes1312@naver.com>
2022-06-10 00:56:38 +0900

    rocksccisorspaper

--- CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study.cpp
@@ -3,51 +3,96 @@ using namespace std;
 
 int main()
 {
-	//exercise1
-	/*int input;
-	cin >> input;
+	srand(time(0)); // 시드 설정
 
-	for (int i = 0; i < input; i++)
-	{
-		for (int j = 0; j < input; j++)
-		{
-			cout << "*";
-		}
-		cout << endl;
-	}*/
-
-	//exercise2
-	/*int input;
-	cin >> input;
-
-	for (int i = 1; i <= input; i++)
-	{
-		for (int j = 0; j < i; j++)
-		{
-			cout << "*";
-		}
-		cout << endl;
-	}*/
+	// 1 2 3
 	
-	//exercise3
-	/*int input;
-	cin >> input;
+	const int SCISSORS = 1;
+	const int ROCK = 2;
+	const int PAPER = 3;
 
-	for (int i = 0; i < input; i++)
+	int wins = 0;
+	int total = 0;
+
+	while (true)
 	{
-		for (int j = 0; j < input - i; j++)
+		cout << "가위(1) 바위(2) 보(3) 골라주세요!" << endl;
+		cout << "> ";
+
+		if (total == 0)
 		{
-			cout << "*";
+			cout << "현재 승률 : 없음" << endl;
 		}
-		cout << endl;
-	}*/
-
-	//exercise4
-	for (int i = 2; i <= 9; i++)
-	{
-		for (int j = 1; j <= 9; j++)
+		else
 		{
-			cout << i << " * " << j << " = " << i * j << endl;
+			
+			cout << "현재 승률 : " << (wins * 100) / total << endl;
+		}
+
+		// 컴퓨터
+		int computerValue = 1 + (rand() % 3);
+
+		// 사용자
+		int input;
+		cin >> input;
+
+		if (input == SCISSORS)
+		{
+			switch (computerValue)
+			{
+			case SCISSORS:
+				cout << "가위(님) vs 가위(컴퓨터) 비겼습니다!" << endl;
+				break;
+			case ROCK:
+				cout << "가위(님) vs 바위 (컴퓨터) 졌습니다!" << endl;
+				total++;
+				break;
+			case PAPER:
+				cout << "가위(님) vs 보 (컴퓨터) 이겼습니다!" << endl;
+				wins++;
+				total++;
+				break;
+			}
+		}
+		else if (input == ROCK)
+		{
+			switch (computerValue)
+			{
+			case SCISSORS:
+				cout << "바위(님) vs 가위(컴퓨터) 이겼습니다!" << endl;
+				total++;
+				wins++;
+				break;
+			case ROCK:
+				cout << "바위(님) vs 바위 (컴퓨터) 비겼습니다!" << endl;
+				break;
+			case PAPER:
+				cout << "바위(님) vs 보 (컴퓨터) 졌습니다!" << endl;
+				total++;
+				break;
+			}
+		}
+		else if (input == PAPER)
+		{
+			switch (computerValue)
+			{
+			case SCISSORS:
+				cout << "보(님) vs 가위(컴퓨터) 졌습니다!" << endl;
+				total++;
+				break;
+			case ROCK:
+				cout << "보(님) vs 바위 (컴퓨터) 이겼습니다!" << endl;
+				total++;
+				wins++;
+				break;
+			case PAPER:
+				cout << "보(님) vs 보 (컴퓨터) 비겼습니다!" << endl;
+				break;
+			}
+		}
+		else
+		{
+			break;
 		}
 	}
 }
