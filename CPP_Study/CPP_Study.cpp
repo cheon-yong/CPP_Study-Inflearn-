83c4b4fc34e8b05250d18a565265cd1f1303997f
cheonyongjun <yes1312@naver.com>
2022-06-09 23:10:49 +0900

    const

--- CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study.cpp
@@ -24,6 +24,29 @@ bool isInvincible = true;
 
 unsigned char flag; // 부호를 없애야 >> 를 하더라도 부호비트가 딸려오지 않음
 
+// 한번 정해지면 절대 바뀌지 않을 값들
+// constant의 약자인 const를 붙임(변수를 상수화한다고 함)
+// const를 붙였으면 초기값을 반드시 지정해야함
+
+const int AIR = 0;
+const int STUN = 1;
+const int POLYMORPH = 2;
+const int INVINCIBLE = 3;
+
+// 전역 변수
+
+// [데이터 영역]
+// .data (초기값이 있는 경우)
+int c = 2;
+
+// .bss (초기값이 없는 경우)
+int d;
+
+// .rodata (읽기 전용 데이터)
+// 사실 C++ 표준에서 꼭 그렇게 하라는 말은 없음
+// 그냥 컴파일러 (VS) 마음이라는 것
+const char* msg = "Hello World";
+
 int main()
 {
 #pragma region 산술 연산
@@ -145,17 +168,18 @@ int main()
 	// 0b0000 [무적][변이][스턴][공중부양]
 
 	// 무적 상태로 만든다
-	flag = (1 << 3); // ??
+	flag = (1 << INVINCIBLE); // ??
 
 	// 변이상태를 추가한다 (무적 + 변이)
-	flag |= (1 << 2);
+	flag |= (1 << POLYMORPH);
 
 	// 무적인지 확인하고 싶다? (다른 상태는 관심없음)
 	// bitmask
-	bool invincible = (flag & (1 << 3)) != 0;
+	bool invincible = (flag & (1 << INVINCIBLE)) != 0;
 
 	// 무적이거나 스턴 인지 확인하고 싶다면?
-	bool mask = (1 << 3) | (1 << 1);
+	bool mask = (1 << INVINCIBLE) | (1 << STUN);
 	bool stunOrInvincible = ((flag & 0b1010) != 0);
 #pragma endregion
+	
 }
