1fb5337096f5c56c06e312f64741e4a7ef9d3f99
cheonyongjun <yes1312@naver.com>
2022-06-14 21:14:18 +0900

    function

--- CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study.cpp
@@ -1,118 +1,57 @@
 ﻿#include <iostream>
 using namespace std;
 
-// 상수지만 너무 따로 노는 느낌
-// 하나의 세트로 묶는다면?
-const int SCISSORS = 1;
-const int ROCK = 2;
-const int PAPER = 3;
+// 오늘의 주제 : 함수 (프로시저, 메소드, 루틴)
+/*
+input 으로 무엇을 받고, output으로 무엇을 뱉을지 정해준다
 
-// 숫자를 지정 안하면 첫 값은 0부터 시작
-// 그 다음 값들은 이전 값 + 1
-enum ENUM_SRP
+반환타입 함수이름 ([인자타입 매개변수])
 {
-	ENUM_SCISSORS = 1,
-	ENUM_ROCK,
-	ENUM_PAPER
-};
+함수 내용
 
-// #이 붙은 것 -> 전처리 지시문
-// #include <iostream>이라는 파일을 찾아서 해당 내용을 복붙
-// 1) 전처리 2) 컴파일 3) 링크
-#define DEFINE_SCISSORS 1
-#define DEFINE_TEST cout << "Hello World" << endl;
+return ~~~;
+}
+*/
+
+// Hello World를 콘솔에 출력하는 함수를 만들어보자!
+// input : 없음 / output: 없음
+// 타입 : int float double char~~
+
+void PrintHelloWorld()
+{
+	cout << "Hello World" << endl;
+}
+
+// 정수를 입력(함수) 받아서, 콘솔에 출력하는 함수를 만들어보자
+// input : int / output : 없음
+
+void PrintNumber(int number)
+{
+	cout << "넘겨주신 숫자는 " << number << "입니다." << endl;
+}
+
+// 2를 곱하고 결과물을 뱉는 함수를 만들어보자
+// input : int / output : int
+
+int MultiplyBy2(int a)
+{
+	int result = a * 2;
+	return result;
+}
+
+// 두 숫자를 곱해서 결과물을 뱉는 함수를 만들어보자
+// input : int, int / output int
+
+int MultiplyBy(int a, int b)
+{
+	return a * b;
+}
 
 int main()
 {
-	DEFINE_TEST;
+	PrintNumber(2);
+	PrintHelloWorld();
 
-	srand(time(0)); // 시드 설정
-
-	// 1 2 3
-	
-
-	int wins = 0;
-	int total = 0;
-
-	while (true)
-	{
-		cout << "가위(1) 바위(2) 보(3) 골라주세요!" << endl;
-		cout << "> ";
-
-		if (total == 0)
-		{
-			cout << "현재 승률 : 없음" << endl;
-		}
-		else
-		{
-			
-			cout << "현재 승률 : " << (wins * 100) / total << endl;
-		}
-
-		// 컴퓨터
-		int computerValue = 1 + (rand() % 3);
-
-		// 사용자
-		int input;
-		cin >> input;
-
-		if (input == ENUM_SCISSORS)
-		{
-			switch (computerValue)
-			{
-			case SCISSORS:
-				cout << "가위(님) vs 가위(컴퓨터) 비겼습니다!" << endl;
-				break;
-			case ROCK:
-				cout << "가위(님) vs 바위 (컴퓨터) 졌습니다!" << endl;
-				total++;
-				break;
-			case PAPER:
-				cout << "가위(님) vs 보 (컴퓨터) 이겼습니다!" << endl;
-				wins++;
-				total++;
-				break;
-			}
-		}
-		else if (input == ENUM_ROCK)
-		{
-			switch (computerValue)
-			{
-			case SCISSORS:
-				cout << "바위(님) vs 가위(컴퓨터) 이겼습니다!" << endl;
-				total++;
-				wins++;
-				break;
-			case ROCK:
-				cout << "바위(님) vs 바위 (컴퓨터) 비겼습니다!" << endl;
-				break;
-			case PAPER:
-				cout << "바위(님) vs 보 (컴퓨터) 졌습니다!" << endl;
-				total++;
-				break;
-			}
-		}
-		else if (input == ENUM_PAPER)
-		{
-			switch (computerValue)
-			{
-			case SCISSORS:
-				cout << "보(님) vs 가위(컴퓨터) 졌습니다!" << endl;
-				total++;
-				break;
-			case ROCK:
-				cout << "보(님) vs 바위 (컴퓨터) 이겼습니다!" << endl;
-				total++;
-				wins++;
-				break;
-			case PAPER:
-				cout << "보(님) vs 보 (컴퓨터) 비겼습니다!" << endl;
-				break;
-			}
-		}
-		else
-		{
-			break;
-		}
-	}
+	int result = MultiplyBy(3, 5);
+	PrintNumber(result);
 }
