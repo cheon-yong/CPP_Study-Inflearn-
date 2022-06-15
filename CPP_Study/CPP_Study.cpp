89f9c156d0dd41d1b4404b1295d762f4116462f8
cheonyongjun <yes1312@naver.com>
2022-06-16 00:26:00 +0900

    array vs pointer

--- CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study.cpp
@@ -1,65 +1,54 @@
 ﻿#include <iostream>
 using namespace std;
 
-// 오늘의 주제 : 배열
+// 오늘의 주제 : 포인터 vs 배열
 
-struct StatInfo
+void Test(int a)
 {
-	int hp = 100; // +0
-	int attack = 10; // +4
-	int defence = 5; // +8
-};
+	a++;
+}
+
+// 배열은 함수 인자로 넘기면, 컴파일러가 알아서  포인터로 치환한다 (char[] -> char*)
+// 배열 내용 전체를 넘긴 것이 아니라 주소를 넘김
+void Test(char a[])
+{
+	a[0] = 'x';
+}
 
 int main()
 {
-	// TYPE 이름[개수];
+	// 문자열 = 문자 배열
+	/*cout << "Hellow World" << endl;
 
-	// 배열의 크기는 상수여야 함 (VC 컴파일러 기준)
-	StatInfo monsters[10];
+	char msg[] = { 'H', 'e', 'l','l', 'o', '\0'};
+	cout << msg << endl;*/
 
-	// 여태껏 변수들의 [이름]은 바구니의 이름이었음
+	// test1[ 주소 ] << 8바이트 (주소크기, 32bit 운영체제는 4바이트)
+	// .data 주소[H][e][l][l].....[d][\0]
+	const char* test1 = "Hello World";
 
-	// 그러나 배열은 다르게 동작
-	// 배열의 이름은 곧 배열의 시작 주소
-	// 정확히는 시작 위치를 가리키는 TYPE* 포인터
+	// .data 주소[H][e][l][l].....[d][\0]
+	// [][][]....[][]  12개짜리 변수배열
+	// [H][e][l][l].....[d][\0]
+	// test2 = 주소
+	char test2[] = "Hello World";
+	test2[0] = 'R';
 
-	auto WhoAmi = monsters;
+	cout << test2 << endl;
 
-	for (int i = 0; i < 10; i++)
-	{
-		StatInfo& monster = *(monsters + i);
-		monster.hp = 100 * (i + 1);
-		monster.attack = 30 * (i + 1);
-		monster.defence = (i + 1);
-	}
+	// 포인터는 [주소를 담는 바구니]
+	// 배열은 [닭장] 즉, 그 자체로 같은 데이터끼리 붙어있는 '바구니 모음'
+	// - 다만 [배열 이름]은 '바구니 모음'의 [시작주소]
 
-	// 근데 *(monsters i) 너무 불편하고 가독성이 떨어진다
-	// 인덱스!
-	// 배열은 0부터 시작 N번째 인덱스에 해당하는 데이터에 접근하려면 배열이름[N]
-	// *(mosters + i) = monsters[i]
+	// 배열을 함수의 인자로 넘기게 되면?
 
-	for (int i = 0; i < 10; i++)
-	{
-		monsters[i].hp = 100 * (i + 1);
-		monsters[i].attack = 30 * (i + 1);
-		monsters[i].defence = (i + 1);
-	}
+	int a = 0;
+	Test(a);
 
-	// 배열 초기화 문법 몇가지
-	int numbers[5] = {}; // 0으로 초기화
-	int numbers1[10] = { 1, 2, 3, 4, 5 }; // 설정하는 애들은 설정한 값들로 나머지 값들은 0으로
-	int numbers2[] = { 1,2,3,4,5,6,11,13,15,1 }; // 데이터 수 만큼의 크기만큼 배열로 생성
+	cout << a << endl;
 
-	char helloStr[] = { 'H', 'e', 'l', 'l', 'o', '\0' };
-
-	// 배열 요약:
-	// 1) 선언한다
-	int arr[10] = {};
-
-	// 2) 인덱스로 접근해서 사용
-	arr[1] = 1;
-	cout << arr[1] << endl;
-
-
+	Test(test2);
+	cout << test2 << endl;
+	
 	return 0;
 }
