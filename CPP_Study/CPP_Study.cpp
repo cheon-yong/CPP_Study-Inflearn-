89fb6ae30a4e2b4d1d7b1656d10a4a2da32d1d8a
전용준 <yes1312@naver.com>
2022-06-16 14:59:57 +0900

    multi array

--- CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study.cpp
@@ -1,44 +1,26 @@
 ﻿#include <iostream>
 using namespace std;
 
-// 오늘의 주제 : 다중 포인터
+// 오늘의 주제 : 다차원 배열
 
-void SetNumber(int* a)
-{
-	*a = 1;
-}
-
-void SetMessage(const char* a)
-{
-	a = "Bye";
-}
-
-void SetMessage(const char** a)
-{
-	*a = "Bye";
-}
-
-void SetMessage2(const char*& a)
-{
-	a = "Wow";
-}
 
 int main()
 {
-	int a = 0;
-	SetNumber(&a);
-	cout << a << endl;
+	int a[10] = { 1, 2, 3 };
 
+	// 
+	int apartment2D[2][5] = { {4, 2, 3, 4, 1}, {1, 1, 5, 2, 2} };
 
-	const char* msg = "Hello";
-	//SetMessage(&msg);
-	//cout << msg << endl;
-
-	const char** pp = &msg;
-	SetMessage(&msg);
-	cout << msg << endl;
+	for (int floor = 0; floor < 2; floor++)
+	{
+		for (int room = 0; room < 5; room++)
+		{
+			int num = apartment2D[floor][room];
+			cout << num << " ";
+		}
+		cout << endl;
+	}
 	
-	SetMessage2(msg);
-	cout << msg << endl;
+	int apartment1D[10] = { 4, 2, 3, 4, 1, 1, 1, 5, 2, 2 }
 	return 0;
 }
