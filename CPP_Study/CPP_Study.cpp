4efd7c9df137e22c5fba0418592ece9342d9563d
전용준 <yes1312@naver.com>
2022-06-16 19:05:48 +0900

    making TextRPG2

--- CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study.cpp
@@ -1,81 +1,157 @@
 ﻿#include <iostream>
 using namespace std;
 
-// 오늘의 주제 : 포인터 마무리
+// 오늘의 주제 : TextRPG2 (Pointer.ver)
 
-// 1) 포인터 vs 배열 2탄
-// 2) 주의사항 
 
-int& TestRef()
+// main
+// - EnterLobby
+// -- CreatePlayer
+// -- EnterGame
+// --- CreateMonsters
+// --- EnterBattle
+
+enum PlayerType
 {
-	int a = 1;
-	return a;
-}
+	PT_Knight = 1,
+	PT_Archer,
+	PT_Mage,
+};
 
-int* TestPointer()
+enum MonsterType 
 {
-	int a = 1;
-	return &a;
-}
+	MT_Slime = 1,
+	MT_Orc,
+	MT_Skeleton,
+};
 
-void TestWrong(int* ptr)
+struct StatInfo
 {
-	int a[100] = {};
-	a[99] = 0xAAAAAAAA;
-	*ptr = 0x12341234;
-}
+	int hp = 0;
+	int attack = 0;
+	int defence = 0;
+};
+
+void EnterLobby();
+void PrintMessage(const char* msg);
+void CreatePlayer(StatInfo* playerInfo);
+void PrintStatInfo(const char* name, const StatInfo& info);
+void EnterGame(StatInfo* playerInfo);
+void CreateMonsters(StatInfo monsterInfo[], int count);
+
+
 int main()
 {
-	// 주소를 담는 바구니
-	// 진짜는 어딘가에
-	// p는 단지 그 곳으로 이동하는 역할
-	int* p;
-
-	// 진짜 데이터의 나열
-	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-	
-	// 그런데 상당히 많은 사람들이 [배열 = 포인터] 라고 착각하는 경향이 있음
-
-	// - [배열의 이름]은 배열의 시작 주소값을 가리키는 TYPE* 포인터로 변환 가능!
-
-	// - [TYPE형 1차원 배열]과 [TYPE* 포인터]는 완전히 호환이 된다.
-	
-	p = arr;
-
-
-	//cout << p[0] << endl;
-	//cout << arr[0] << endl;
-
-	//cout << *p << endl;
-	//cout << *arr << endl;
-
-	//cout << *(p + 3) << endl;
-	//cout << *(arr + 3) << endl;
-
-	// 그 런 데
-	
-	// [1][2][3][4]
-	int arr2[2][2] = { {1, 2}, {3, 4} };
-
-	//int** pp = (int**)arr2;
-
-	int(*p2)[2] = arr2;
-
-	cout << (*p2)[0] << endl;
-	cout << (*p2)[1] << endl;
-	
-	cout << (*(p2 + 1))[0] << endl;
-	cout << (*(p2 + 1))[1] << endl;
-
-	cout << p2[0][0] << endl;
-	cout << p2[0][1] << endl;
-	cout << p2[1][0] << endl;
-	cout << p2[1][1] << endl;
-
-	int* pointer = TestPointer();
-	TestWrong(pointer);
-	
-	//cout << (**pp) << endl;
-
+	srand((unsigned)time(nullptr));
+	EnterLobby();
 	return 0;
+}
+
+void EnterLobby()
+{
+	while (true)
+	{
+		PrintMessage("로비에 입장했습니다.");
+
+		// Player
+		StatInfo playerInfo;
+		CreatePlayer(&playerInfo);
+		PrintStatInfo("Player", playerInfo);
+		EnterGame(&playerInfo);
+	}
+}
+
+void PrintMessage(const char* msg)
+{
+	cout << "******************" << endl;
+	cout << msg << endl;
+	cout << "******************" << endl;
+}
+
+void CreatePlayer(StatInfo* playerInfo)
+{
+	bool ready = false;
+	while (ready == false)
+	{
+		PrintMessage("캐릭터 생성창");
+		PrintMessage("[1] 기사 [2] 궁수 [3] 법사");
+		cout << "> ";
+
+		int input;
+		cin >> input;
+
+		switch (input)
+		{
+		case PT_Knight:
+			playerInfo->hp = 100;
+			playerInfo->attack = 10;
+			playerInfo->defence = 10;
+			ready = true;
+			break;
+		case PT_Archer:
+			playerInfo->hp = 80;
+			playerInfo->attack = 15;
+			playerInfo->defence = 5;
+			ready = true;
+			break;
+		case PT_Mage:
+			playerInfo->hp = 60;
+			playerInfo->attack = 20;
+			playerInfo->defence = 3;
+			ready = true;
+			break;
+		}
+	}
+}
+
+void PrintStatInfo(const char* name, const StatInfo& info)
+{
+	cout << "******************" << endl;
+	cout << name << "HP=" << info.hp << " ATT=" << info.attack << " DEF=" << info.defence << endl;
+	cout << "******************" << endl;
+}
+
+void EnterGame(StatInfo* playerInfo)
+{
+	const int MONSTER_COUNT = 2;
+	PrintMessage("게임에 입장했습니다");
+
+	while (true)
+	{
+		StatInfo monsterInfo[MONSTER_COUNT];
+		CreateMonsters(monsterInfo, MONSTER_COUNT);
+
+		for (int i = 0; i < MONSTER_COUNT; i++)
+			PrintStatInfo("Monster", monsterInfo[i]);
+
+		PrintStatInfo("Player", *playerInfo);
+
+		break;
+	}
+}
+
+void CreateMonsters(StatInfo monsterInfo[], int count)
+{
+	for (int i = 0; i < count; i++)
+	{
+		int randValue = 1 + rand() % 3;
+		switch (randValue)
+		{
+		case MT_Slime:
+			monsterInfo[i].hp = 30;
+			monsterInfo[i].attack = 5;
+			monsterInfo[i].defence = 1;
+			break;
+		case MT_Orc:
+			monsterInfo[i].hp = 40;
+			monsterInfo[i].attack = 8;
+			monsterInfo[i].defence = 2;
+			break;
+		case MT_Skeleton:
+			monsterInfo[i].hp = 50;
+			monsterInfo[i].attack = 10;
+			monsterInfo[i].defence = 3;
+			break;
+		}
+	}
 }
