aa8172a9e8dd6fd73b19c607a5eec6c031cae3e6
전용준 <yes1312@naver.com>
2022-06-16 12:48:46 +0900

    multi pointer

--- CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study.cpp
@@ -1,68 +1,44 @@
 ﻿#include <iostream>
 using namespace std;
 
-// 오늘의 주제 : 로또 번호 생성기
+// 오늘의 주제 : 다중 포인터
 
-// 1) Swap 만들기
-void Swap(int* a, int* b)
+void SetNumber(int* a)
 {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
+	*a = 1;
 }
 
-
-// 2) Sort 만들기
-void Sort(int numbers[],int count)
+void SetMessage(const char* a)
 {
-	for (int i = 0; i < count; i++)
-	{
-		for (int j = i; j < count; j++)
-		{
-			if (numbers[i] > numbers[j])
-				Swap(&numbers[i], &numbers[j]);
-		}
-	}
+	a = "Bye";
 }
 
-// 3) 로또 번호 생성
-void ChooseLotoo(int numbers[])
+void SetMessage(const char** a)
 {
-	// TODO : 랜덤으로 1 ~ 45 사이의 숫자 6개를 골라주세요!
-	int madeNumber = 0;
-	while (madeNumber < 6)
-	{
-		int num = (rand() % 45) + 1;
-
-		bool isExist = false;
-		for (int i = 0; i < madeNumber; i++)
-		{
-			if (numbers[i] == num)
-			{
-				isExist = true;
-				break;
-			}
-		}
-
-		if (isExist)
-			continue;
-
-		numbers[madeNumber] = num;
-		madeNumber++;
-	}
-
-	Sort(numbers, 6);
+	*a = "Bye";
 }
+
+void SetMessage2(const char*& a)
+{
+	a = "Wow";
+}
+
 int main()
 {
-	srand(time(0));
-	int numbers[6] = {};
-	ChooseLotoo(numbers);
+	int a = 0;
+	SetNumber(&a);
+	cout << a << endl;
 
-	for (int i = 0; i < 6; i++)
-	{
-		cout << numbers[i] << " ";
-	}
-	cout << endl;
+
+	const char* msg = "Hello";
+	//SetMessage(&msg);
+	//cout << msg << endl;
+
+	const char** pp = &msg;
+	SetMessage(&msg);
+	cout << msg << endl;
+	
+	SetMessage2(msg);
+	cout << msg << endl;
 	return 0;
 }
