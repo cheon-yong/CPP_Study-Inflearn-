9c247a632891c3170cfa5e246754c71240a4c3ec
전용준 <yes1312@naver.com>
2022-06-17 12:13:19 +0900

    exercise snail

--- CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study.cpp
@@ -1,92 +1,67 @@
 ﻿#include <iostream>
+#include <iomanip>
 using namespace std;
 
-// 오늘의 주제 : 연습문제 (문자열) #1
+// 오늘의 주제 : 연습문제
 
-// 문제1) 문자열 길이를 출력하는 함수
-int StrLen(const char* str)
+const int MAX = 100;
+int board[100][100] = {};
+int N;
+
+int dy[4] = {0, 1, 0, -1};
+int dx[4] = {1, 0, -1, 0};
+
+void PrintBoard()
 {
-	int len = 0;
-	while (str[len] != 0)
+	for (int y = 0; y < N; y++)
 	{
-		len++;
-	}
-
-	return len;
-}
-
-// 문제2) 문자열 복사 함수
-void StrCpy(char* dest, char* src)
-{
-	int i = 0;
-	while (src[i] != 0)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = 0;
-}
-
-// 문제3) 문자열 덧붙이는 함수
-void StrCat(char* dest, char* src)
-{
-	int i = 0;
-	while (*dest)
-		dest++;
-
-	while (src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = 0;
-}
-
-// 문제4) 문자열 비교
-int StrCmp(char* a, char* b)
-{
-	int i = 0;
-	while (a[i] && b[i])
-	{
-		if (a[i] > b[i])
-			return 1;
-		if (a[i] < b[i])
-			return -1;
-			
-		i++;
-	}
-
-	return 0;
-}
-
-// 문제5) 문자열을 뒤집는 함수
-void ReverseStr(char* str)
-{
-	int len = StrLen(str);
-
-	for (int i = 0; i < len / 2; i++)
-	{
-		int temp = str[i];
-		str[i] = str[len - i - 1];
-		str[len - i - 1] = temp;
+		for (int x = 0; x < N; x++)
+		{
+			cout << setfill('0') << setw(2) << board[y][x] << " ";
+		}
+		cout << endl;
 	}
 }
 
-#pragma warning(disable: 4996)
+enum DIR
+{
+	RIGHT = 0,
+	DOWN,
+	LEFT,
+	UP,
+};
+
+void SetBoard()
+{
+	int value = 1;
+	int dest = N * N;
+	int direct = RIGHT;
+	int x = 0;
+	int y = 0;
+	while (value <= dest)
+	{
+		board[y][x] = value;
+		value++;
+
+		int nextX = x + dx[direct];
+		int nextY = y + dy[direct];
+
+		if (nextX == N || nextY == N || nextX < 0 || nextY < 0 || board[nextY][nextX] != 0)
+		{
+			direct = (direct + 1) % 4;
+		}
+
+		x += +dx[direct];
+		y += +dy[direct];
+	}
+}
+
 int main()
 {
-	const int BUF_SIZE = 100;
+	cin >> N;
 
-	char a[BUF_SIZE] = "Hello";
-	int len = StrLen(a);
-	cout << len << endl;
-	char b[BUF_SIZE] = "Hello";
-	// StrCpy(b, a);
-	
-	//StrCat(a, b);
-	ReverseStr(a);
-	cout << a << endl;
-	//cout << StrCmp(a, b) << endl;
+	SetBoard();
 
+	PrintBoard();
 	return 0;
 }
