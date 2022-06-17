ab0d479125f7bd1342df1ade296e3c01d8cf4faa
전용준 <yes1312@naver.com>
2022-06-17 18:52:32 +0900

    Polymorphism #2

--- CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study.cpp
@@ -21,13 +21,30 @@ using namespace std;
 // 일반 함수는 정적 바인딩을 사용
 // 동적 바인딩을 원하면다면? -> 가상 함수 (virtual function)
 
+// 그런데 실제 객체가 어떤 타입인지 어떻게 알고 알아서가상함수를 호출해 준 걸까?
+// - 가상 함수 테이블 (vftable)
+
+// .vftable 주소테이블! [] 4바이트 (32) 8바이트 (64)
+
+// [VMove] [VDie]
+
+// 순수 가상 함수 : 구현은 없고 '인터페이스'만 전달하는 용도로 사용하고 싶을 경우
+// 추상 클래스 : 순수 가상 함수가 1개 이상 포함되면 바로 추상 클래스로 간주
+// - 직접적으로 객체를 만들 수 없게 됨
 
 class Player
 {
 public:
+	Player()
+	{
+		_hp = 100;
+	}
 	void Move() { cout << "Move Player!" << endl; }
 	//void Move(int a) { cout << "Mover Player (int) !" << endl; }
-	virtual void VMove() { cout << "Move Player!" << endl; }
+	virtual void VMove() { cout << "VMove Player!" << endl; }
+	virtual void VDie() { cout << "VDie Player!" << endl; }
+	
+	virtual void VAttack() = 0;
 public:
 	int _hp;
 };
@@ -35,8 +52,16 @@ public:
 class Knight : public Player
 {
 public:
+	Knight()
+	{
+		_stamina = 100;
+	}
 	void Move() { cout << "Move Knight!" << endl; }
-	virtual void VMove() { cout << "Move Knight!" << endl; }
+	virtual void VMove() { cout << "VMove Knight!" << endl; }
+	virtual void VDie() { cout << "VDie Knight!" << endl; }
+
+	virtual void VAttack() { cout << "VAttack Knight" << endl; }
+	
 public:
 	int _stamina;
 };
@@ -45,21 +70,27 @@ class Mage : public Player
 {
 public:
 	int _mp;
+	virtual void VAttack() { cout << "VAttack Mage" << endl; }
 };
+
+// [ [ Player ] ]
+// [   Knight   ]
 
 void MovePlayer(Player* player)
 {
 	player->VMove();
+	player->VDie();
 }
 
 int main()
 {
 	// 자식클래스에서 부모클래스는 되지만
 	// 부모클래스에서 자식클래스는 쉽지 않음
-	Player p;
-	MovePlayer(&p); // 플레이어는 플레이어다? YES
+	//Player p;
+	//MovePlayer(&p); // 플레이어는 플레이어다? YES
 	//MoveKnight(&p); // 플레이어는 기사다? NO
 
+	cout << endl;
 
 	Knight k;
 	//MoveKnight(&k); // 기사는 기사다? Yes
