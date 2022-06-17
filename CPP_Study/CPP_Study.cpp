ddf5579efe988333b23fff686bb4e65cfa8a2f4b
전용준 <yes1312@naver.com>
2022-06-17 11:46:56 +0900

    string exercise#2

--- CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study.cpp
@@ -27,7 +27,50 @@ void StrCpy(char* dest, char* src)
 	dest[i] = 0;
 }
 
-//
+// 문제3) 문자열 덧붙이는 함수
+void StrCat(char* dest, char* src)
+{
+	int i = 0;
+	while (*dest)
+		dest++;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = 0;
+}
+
+// 문제4) 문자열 비교
+int StrCmp(char* a, char* b)
+{
+	int i = 0;
+	while (a[i] && b[i])
+	{
+		if (a[i] > b[i])
+			return 1;
+		if (a[i] < b[i])
+			return -1;
+			
+		i++;
+	}
+
+	return 0;
+}
+
+// 문제5) 문자열을 뒤집는 함수
+void ReverseStr(char* str)
+{
+	int len = StrLen(str);
+
+	for (int i = 0; i < len / 2; i++)
+	{
+		int temp = str[i];
+		str[i] = str[len - i - 1];
+		str[len - i - 1] = temp;
+	}
+}
 
 #pragma warning(disable: 4996)
 int main()
@@ -37,12 +80,13 @@ int main()
 	char a[BUF_SIZE] = "Hello";
 	int len = StrLen(a);
 	cout << len << endl;
-	char b[BUF_SIZE] = "World";
+	char b[BUF_SIZE] = "Hello";
 	// StrCpy(b, a);
 	
-	cout << b << endl;
-	len = StrLen(a);
-	cout << len << endl;
+	//StrCat(a, b);
+	ReverseStr(a);
+	cout << a << endl;
+	//cout << StrCmp(a, b) << endl;
 
 	return 0;
 }
