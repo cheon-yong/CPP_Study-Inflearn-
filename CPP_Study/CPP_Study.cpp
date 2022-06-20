c0712b20517856d4209c17dd2a0b2c32009e2594
전용준 <yes1312@naver.com>
2022-06-20 12:26:16 +0900

    operator overloading

--- CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study.cpp
@@ -3,80 +3,98 @@
 using namespace std;
 
 
-// 오늘의 주제 : 초기화 리스트
+// 오늘의 주제 : 연산자 오버로딩 (Operator Overloading)
 
-// 멤버 변수 초기화 ? 다양한 문법이 존재
-
-// 초기화 왜 해야할까? 귀찮은데
-// - 버그 예방에 중요
-// - 포인터 등 주소 값이 연루되어 있을 경우
-
-// 초기화 방법
-// - 생성자 내에서
-// - 초기화 리스트
-// - C++11 문법
-
-// 초기화 리스트
-// - 상속관계에서 원하는 부모 생성자 호출을 할 때 필요
-// - 생성자 내에서 초기화 vs 초기화 리스트
-// -- 일반 변수는 차이 없음
-// -- 멤버 타입이 클래스인 경우 차이가 남
-// -- 정의함과 동시에 초기화가 필요한 경우 (참조타입, const 타입)
-
-
-class Inventory
-{
-public :
-	Inventory() { cout << "Inventory()" << endl; }
-	Inventory(int size) { cout << "Inventory(int size)" << endl; _size = size; }
-	~Inventory() { cout << "~Inventory()" << endl; }
-public :
-	int _size = 10;
-};
-
-class Player
-{
-public:
-	Player() { }
-	Player(int id) { }
-};
-
-// Is-A (Knight Is-A Player?) 기사는 플레이어다 OK -> 상속관계
+// 연산자 vs 함수
+// - 연산자는 피연산자의 개수/타입이 고정되어 있음
 // 
-// Has-A (Knight Has-A Inventory?) 기사는 인벤토리냐? NO -> 포함관계
 
-class Knight : public Player
+// 연산자 오버로딩?
+// 일단 [연산자 함수]를 정의해야 함
+// 함수도 멤버함수 vs 전역함수가 존재하는 것처럼, 연산자 함수도 두 가지 방식으로 가능
+
+// - 멤버 연산자 함수 version
+// -- a op b 형태에서 왼쪽을 기준으로 실행됨 (a가 클래스여야 가능. a를 '기준 피연산자'라고 함)
+// -- 한계) a가 클래스가 아니면 사용 못함
+
+// - 전역 연산자 함수 version
+// -- a op b 형태라면 a, b 모두를 연산자 함수의 피연산자로 만들어준다.
+
+// 무엇이 더 좋은가? -> 의미 없음. 심지어 둘 중 하나만 지원하는 경우도 있기 때문
+// - 대표적으로 대입 연산자(a = b)는 전역 연산자 버전으로는 만들 수 없음
+
+class Position
 {
 public:
-	Knight() : Player(1), _hp(100), _inventory(20), _hpRef(_hp), _hpConst(100)
-		/*
-		* 선처리영역
-		* 
-		*/
+	Position operator+ (const Position& arg)
 	{
-		_hp = 100;
+		Position pos;
+		pos._x = _x + arg._x;
+		pos._y = _y + arg._y;
+		return pos;
 	}
 
-public:
-	int _hp; // 쓰레기값
-	int _stamina = 100; // C++ 11 문법
-	Inventory _inventory;
+	Position operator+ (int arg)
+	{
+		Position pos;
+		pos._x = _x + arg;
+		pos._y = _y + arg;
+		return pos;
+	}
 
-	int& _hpRef;
-	const int _hpConst;
+	bool operator== (const Position& arg)
+	{
+		return _x == arg._x && _y == arg._y;
+	}
+
+	void operator=(int arg)
+	{
+		_x = arg;
+		_y = arg;
+	}
+public:
+	int _x;
+	int _y;
+
 };
+
+
+Position operator+ (int a, const Position& b)
+{
+	Position ret;
+
+	ret._x = b._x + a;
+	ret._y = b._y + a;
+
+	return ret;
+}
 
 
 int main()
 {
-	Knight k;
+	int a = 1;
+	int b = 2;
 
-	cout << k._hp << endl;
+	int c = a + 3.0f;
 
-	if (k._hp < 0)
-	{
-		cout << "Knight is Dead" << endl;
-	}
+	Position pos;
+	pos._x = 0;
+	pos._y = 0;
+
+	Position pos2;
+	pos2._x = 1;
+	pos2._y = 1;
+
+	Position pos3 = pos + pos2;
+	// == pos3 = pos.operator+(pos2);
+
+	Position pos4 = pos3 + 1;
+	Position pos5 = 1 + pos3; //아직 안됨
+	
+	bool isSame = (pos3 == pos4);
+
+	Position pos6;
+	pos6 = 6;
 
 	return 0;
 }
