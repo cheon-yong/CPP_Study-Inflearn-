8ccdcce59dd20891c9052d28702be2ad016e1731
전용준 <yes1312@naver.com>
2022-06-20 15:13:13 +0900

    struct vs class, static

--- CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study.cpp
@@ -3,185 +3,90 @@
 using namespace std;
 
 
-// 오늘의 주제 : 연산자 오버로딩 (Operator Overloading)
+// 오늘의 주제 : 객체 지향 마무리
 
-// 연산자 vs 함수
-// - 연산자는 피연산자의 개수/타입이 고정되어 있음
-// 
+// 1) struct vs class
 
-// 연산자 오버로딩?
-// 일단 [연산자 함수]를 정의해야 함
-// 함수도 멤버함수 vs 전역함수가 존재하는 것처럼, 연산자 함수도 두 가지 방식으로 가능
+// C++ 에서는 struct 나 class 나 큰 차이가 없음
+// struct는 기본 접근 지정자가 public이고, class는 private이다
+// C++는 C언어에서 파생되어 발전했기 때문에 호환성을 지키기 위함
+// -> struct는 그냥 주고체 (데이터 묶음)을 표현하는 용도
+// -> class 객체 지향 프로그래밍의 특징을 나타내는 용도
 
-// - 멤버 연산자 함수 version
-// -- a op b 형태에서 왼쪽을 기준으로 실행됨 (a가 클래스여야 가능. a를 '기준 피연산자'라고 함)
-// -- 한계) a가 클래스가 아니면 사용 못함
 
-// - 전역 연산자 함수 version
-// -- a op b 형태라면 a, b 모두를 연산자 함수의 피연산자로 만들어준다.
 
-// 무엇이 더 좋은가? -> 의미 없음. 심지어 둘 중 하나만 지원하는 경우도 있기 때문
-// - 대표적으로 대입 연산자(a = b)는 전역 연산자 버전으로는 만들 수 없음
-
-// 복사 대입 연산자
-// 용어가 헷갈림 [복사 생성자] [대입 연산자] [복사 대입 연산자]
-// - 복사 대입 연산자 = 대입 연산자 중, 자기 자신의 참조 타입을 인자로 받는 것
-
-// 기타
-// - 모든 연산자를 다 오버로딩 할 수 있는것은 아님 ("" . .* 이런건 불가)
-// - 모든 연산자가 다 2개 항이 있는 것은 아님 ++ -- 가 대표적(단항 연산자)
-// - 증감 연산자 ++ --
-// -- 전위 형 (++a) operator++()
-// -- 후위 형 (a++) operator++(int)
-class Position
+struct TestStruct
 {
-public:
+	int _a;
+	int _b;
+};
 
-	// 복사 생성자
-	/*Position(const Position& arg)
-	{
-
-	}*/
-
-	Position operator+ (const Position& arg)
-	{
-		Position pos;
-		pos._x = _x + arg._x;
-		pos._y = _y + arg._y;
-		return pos;
-	}
-
-	Position operator+ (int arg)
-	{
-		Position pos;
-		pos._x = _x + arg;
-		pos._y = _y + arg;
-		return pos;
-	}
-
-	bool operator== (const Position& arg)
-	{
-		return _x == arg._x && _y == arg._y;
-	}
-
-	Position& operator=(int arg)
-	{
-		_x = arg;
-		_y = arg;
-
-		return *this;
-	}
-
-	// [복사 생성자] [복사 대입 연산자] 등 특별 대우 받는 이유는
-	// 말 그대로 객체가 '복사'되길 원하는 특징 때문
-	// TODO ) 동적할당 시간에 더 자세히
-	// [복사 대입 연산자]
-	Position& operator=(const Position& arg)
-	{
-		_x = arg._x;
-		_y = arg._y;
-
-		return *this;
-	}
-
-	Position& operator++()
-	{
-		_x++;
-		_y++;
-		return *this;
-	}
-	
-	Position operator++(int)
-	{
-		Position ret = *this;
-		_x++;
-		_y++;
-
-		return ret;
-	}
-
-	void Print()
-	{
-		cout << _x << " " << _y << endl;
-	}
-
-public:
-	int _x;
-	int _y;
-
+class TestClass
+{
+	int _a;
+	int _b;
 };
 
 
-Position operator+ (int a, const Position& b)
+// 2) static 변수, static 함수 (static = 정적 = 고정된)
+
+class Marine
 {
-	Position ret;
+public:
+	int _hp;
 
-	ret._x = b._x + a;
-	ret._y = b._y + a;
+	void TakeDamage(int damage)
+	{
+		_hp -= damage;
+	}
 
-	return ret;
+	static void SetAttack()
+	{
+		s_attack = 100;
+	}
+
+	// 특정 마린 객체와는 무관
+	// 마린이라는 '클래스' 자체와 연관
+	static int s_attack; // 설계도상으로만 존재
+};
+
+int Marine::s_attack = 0;
+
+class Player
+{
+public:
+	int _id;
+};
+
+int GenerateId()
+{
+	// 생명주기 : 프로그램 시작/종료 (메모리에 항상 올라가 있음)
+	// 가시범위 : 
+
+	// 정적 지역 객체, 처음 호출 됐을 때만 초기화되고 이후 유지
+	//1 2 3 4 5...
+	static int s_id = 1;
+
+
+	return s_id++;
 }
-
 
 int main()
 {
-	int a = 1;
-	int b = 2;
+	Marine m1;
+	m1._hp = 40;
+	Marine::s_attack = 6;
 
-	int c = a + 3.0f;
-	
+	Marine m2;
+	m2._hp = 40;
 
-	Position pos;
-	pos._x = 0;
-	pos._y = 0;
-	cout << "pos" << endl;
-	pos.Print();
+	// 마린 공격력 업그레이드 완료! (Academy에서 업그레이드 끝!)
+	Marine::s_attack = 7;
+	Marine::SetAttack();
 
-	Position pos2;
-	pos2._x = 1;
-	pos2._y = 1;
-	cout << "pos2" << endl;
-	pos2.Print();
-
-	Position pos3 = pos + pos2;
-	cout << "pos3" << endl;
-	pos3.Print();
-	// == pos3 = pos.operator+(pos2);
-
-	Position pos4 = pos3 + 1;
-	cout << "pos4" << endl;
-	pos4.Print();
-
-	Position pos5 = 1 + pos3; //아직 안됨
-	cout << "pos5" << endl;
-	pos5.Print();
-
-	bool isSame = (pos3 == pos4);
-
-	Position pos6;
-	pos6 = 6;
-	cout << "pos6" << endl;
-	pos6.Print();
-
-	Position pos7;
-	pos7._x = 0;
-	pos7._y = 0;
-	pos7++;
-	cout << "pos7" << endl;
-	pos7.Print();
-	
-	Position pos8;
-	pos8._x = 0;
-	pos8._y = 0;
-	++pos8;
-	cout << "pos8" << endl;
-	pos8.Print();
-
-
-	Position pos9;
-	cout << "pos9" << endl;
-	pos9 = pos7++;
-	pos9.Print();
+	// 스택 아님. .data 영역
+	static int id = 10;
+	int a = id;
 
 	return 0;
 }
