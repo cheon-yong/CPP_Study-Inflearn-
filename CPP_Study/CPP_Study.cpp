123b2188417ef5df48fe443514b407ffe7476b8e
전용준 <yes1312@naver.com>
2022-06-20 17:39:04 +0900

    type cast #2

--- CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study.cpp
@@ -16,13 +16,30 @@ public:
 class Dog
 {
 public :
+	Dog()
+	{
+
+	}
+	// 타입 변환 생성자
 	Dog(const Knight& knight)
 	{
 		_age = knight._hp;
 	}
+
+	// 타입 변환 연산자
+	operator Knight()
+	{
+		return (Knight)(*this);
+	}
 public:
 	int _age = 1;
 	int _cuteness = 2;
+};
+
+class BullDog : public Dog
+{
+public:
+	bool _french;
 };
 
 
@@ -96,8 +113,53 @@ int main()
 	{
 		Knight knight;
 		Dog dog = (Dog)knight;
+
+		Knight knight2 = dog;
 	}
 
+	// [2] 연관 없는 클래스 사이의 참조 타입 변환
+	// 특징) 명시적으로 가능
+	{
+		Knight knight;
+		// 어셈블리 : 포인터 = 참조
+
+		// [ 주소 ] -> [ Dog ]
+		Dog& dog = (Dog&)knight;
+		dog._cuteness = 12;
+	}
+
+	// ----------------- 상속 관계에 있는 클래스 사이의 변환-------------------
+	// [1] 상속 관계 클래스의 값 타입 변환
+	// 특징) 자식->부모 OK / 부모->자식 NO
+	{
+		Dog dog;
+		//BullDog bulldog = (BullDog)dog; // 어찌해도 안됨
+
+		BullDog bulldog;
+		Dog dog = bulldog;
+	}
+
+	// [2] 상속 관계 클래스의 참조 타입 변환
+	// 특징) 자식 -> 부모 OK / 부모->자식 (암시적NO) (명시적OK)
+	{
+		//Dog dog;
+		//BullDog& bulldog = (BullDog&)dog; // 암튼 됨
+
+		// [ age cuteness french]
+		BullDog bulldog;
+		Dog& dog = bulldog;
+	}
+
+	// 결론)
+	// [값 타입 변환] : 진짜 비트열도 바꾸고 논리적으로말이 되게 바꾸는 변환
+	// - 논리적으로 말이 된다? (ex.BullDog -> Dog) OK
+	// - 논리적으로 말이 안된다 (ex. Dog -> BullDog, Dog -> Knight) 안됨
+	// [참조 타입 변환] : 비트열은 냅두고 우리의 '관점'만 바꾸는 변환
+	// - 땡깡 부리면 (명시적 요구) 해주긴 하는데, 말 안 해도 '그냥' (암시적)으로 해주지는 안전성 여부와 연관 있음
+	// -- 안전하다? (ex.BullDog -> Dog&) '그냥' (암시적으로) OK
+	// -- 위험하다? (ex.Dog -> BullDog&)
+	// --- 메모리 침범 위험이 있는 경우는 '그냥' (암시적으로) 해주진 않음 (위험하니까)
+	// --- 명시적으로 정말 정말 하겠다고 최종 서명을 하면 OK
 
 	return 0;
 }
