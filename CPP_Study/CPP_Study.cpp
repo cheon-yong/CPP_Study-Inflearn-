c611aca3d9d0c6b01ce10867b4a8f12c39021b9f
전용준 <yes1312@naver.com>
2022-06-20 16:20:18 +0900

    dynamic allocation #2

--- CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study.cpp
@@ -30,7 +30,15 @@ using namespace std;
 // - 필요할 때만 사용하고, 필요없으면 반납할 수 있는 것?
 // - 그러면서도 (스택과는 다르게) 우리가 생성/소멸 시점을 관리할 수 있는 것?
 // - 그런 메모리 영역이 HEAP
-// 동적할당과 연관된 함수/연산자 : malloc, free, new, delete, new[], delete[]
+// 동적할당과 연관된 함수/연산자 : malloc / free, new / delete, new[] / delete[]
+
+// malloc
+// - 할당할 메모리 크기를 건내준다
+// - 메모리 할당 후 시작 주소를 가리키는 포인터를 반환해준다 (메모리 부족 NULL)
+
+// free
+// - malloc (혹은 기타 calloc, realloc 등의 사촌) 을 통해 할당된 영역을 해제
+// - 힙 관리자가 할당/미할당 여부를 구분해서 관리
 
 class Monster
 {
@@ -50,8 +58,40 @@ int main()
 	// 커널 영역) 메모리 할당해서 건내줌
 	// 유저 영역) 잘 쓰겠습니다~
 
-	// [                        ]
+	// [ [12][        ] ]
 	// C++ 에서는 기본적으로 CRT(C런타임 라이브러리)의 [힙 관리자]를 통해 힙 영역 사용
 	// 단, 정말 원한다면 우리가 직접 API를 통해 힙을 생성하고 관리할 수도 있음 (MMORPG 서버 메모리 풀링)
+
+	// 그런데 void* 란 무엇일까
+	// *가 있으니까 포인터는 포인터 (주소를 담는 바구니) => OK
+	// 타고가면 void 아무것도 없다? => NO
+	// 타고 가면 void 뭐가 있는지 모르겠으니까 너가 적당히 변환해서 사용해라 => OK
+
+	void* pointer = malloc(sizeof(Monster));
+
+	Monster* m1 = (Monster*)pointer;
+	m1->_hp = 100;
+	m1->_x = 1;
+	m1->_y = 2;
+
+	// Heap Overflow
+	// - 유효한 힙 범위를 초과해서 사용하는 문제
+
+	free(pointer);
+	pointer = nullptr;
+	m1 = nullptr;
+	// free를 안한다면? 메모리 누수
+	// free를 두번한다면? double free
+	// - 이건 대부분 그냥 크래시만 나고 끝남
+
+	// Use-After-Free 심각한 문제 발생
+	// - 프로그래머 입장 : 큰일이 발생함
+	// - 해커 입장 : 취약점 발견
+
+
+	m1->_hp = 100;
+	m1->_x = 1;
+	m1->_y = 2;
+
 	return 0;
 }
