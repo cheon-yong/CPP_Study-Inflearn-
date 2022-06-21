7a58f4ac858dda5576311fce292f4c16b01847fd
cheon-yong <yes1312@naver.com>
2022-06-21 11:52:35 +0900

    Debug

--- CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study.cpp
@@ -1,23 +1,26 @@
 ﻿#include <iostream>
 #include <stdlib.h>
 using namespace std;
-#include "Player.h"
 
+// 오늘의 주제 : 디버깅
 
-// 오늘의 주제 : 전방선언
+void Test2()
+{
+	int b = 0;
+}
 
+void Test1()
+{
+	int a = 0;
+	Test2();
+}
 
 
 int main()
 {
-	// Player는 몇 바이트?
-	// int 2개 = 2 * 4 = 8바이트
+	int number = 10;
 
-	Player p1; // 지역 변수
-	
-	Player* p2 = new Player();
-
-	p1._target = p2;
+	Test1();
 
 	return 0;
 }
