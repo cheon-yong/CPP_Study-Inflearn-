0d496fed8aa2e89674bd547d948de628d3dd0284
cheonyongjun <yes1312@naver.com>
2022-06-28 23:17:11 +0900

    practice

--- CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study.cpp
@@ -5,106 +5,68 @@ using namespace std;
 #include <deque>
 #include <map>
 #include <set>
-// 오늘의 주제 : set, multimap, multiset
+// 오늘의 주제 : 연습 문제
 
 int main()
 {
-	// map은 (key, value) 형태
+	srand(static_cast<unsigned int>(time(nullptr)));
 
-	// set은 key = value
-	set<int> s;
+	vector<int> v;
 
-	// 넣고
-	s.insert(10);
-	s.insert(30);
-	s.insert(20);
-	s.insert(50);
-	s.insert(40);
-	s.insert(70);
-	s.insert(90);
-	s.insert(80);
-	s.insert(100);
-	// 빼고
-	s.erase(40);
-	// 찾고
-	set<int>::iterator findIt = s.find(20);
-	if (findIt == s.end())
+	for (int i = 0; i < 100; i++)
 	{
-		cout << "못 찾음" << endl;
+		int num = rand() % 100;
+		v.push_back(num);
 	}
-	else
+
+	// Q1) number라는 숫자가 벡터에 체크하는 기능 (bool, 첫 등장 iterator)
 	{
-		cout << "찾음" << endl;
+		int number = 50;
+		bool found = false;
+		vector<int>::iterator it;
+
+		// TODO
+		for (it = v.begin(); it != v.end(); ++it)
+		{
+			if ((*it) == number)
+			{
+				found = true;
+				break;
+			}
+		}
 	}
-	// 순회하고
-	for (set<int>::iterator it = s.begin(); it != s.end(); ++it)
-	{
-		cout << (*it) << endl;
-	}
-	cout << "--------------------------" << endl;
-
-	multimap<int, int> mm;
-
-	// 넣고
-	mm.insert(make_pair(1, 100));
-	mm.insert(make_pair(1, 200));
-	mm.insert(make_pair(1, 300));
-	mm.insert(make_pair(2, 400));
-	mm.insert(make_pair(2, 500));
-
-	// mm[1] = 500;
-
-	// 빼고
-	unsigned int count = mm.erase(1);
-
-	// 찾고
-	/*multimap<int, int>::iterator itFind = mm.find(1);
-	if (itFind != mm.end())
-	{
-		mm.erase(itFind);
-	}*/
-
-	pair<multimap<int, int>::iterator, multimap<int, int>::iterator> itPair;
-	itPair = mm.equal_range(1);
-
-	multimap<int, int>::iterator itBegin = mm.lower_bound(1);
-	multimap<int, int>::iterator itEnd = mm.upper_bound(1);
-
-
-	for (multimap<int, int>::iterator it = itPair.first; it != itPair.second; ++it)
-	{
-		cout << it->first << " " << it->second << endl;
-	}
-	cout << "--------------------------" << endl;
-
-	multiset<int> ms;
-
-	// 넣고
-	ms.insert(100);
-	ms.insert(100);
-	ms.insert(100);
-	ms.insert(200);
-	ms.insert(200);
-
-	// 찾고
-	multiset<int>::iterator findIt2 = ms.find(100);
-
-	pair<multiset<int>::iterator, multiset<int>::iterator> itPair2;
-	itPair2 = ms.equal_range(100);
 	
-	for (multiset<int>::iterator it = itPair2.first; it != itPair2.second; ++it)
+	// Q2) 11로 나뉘는 숫자가 벡터에 있는지 체크하는 기능 (bool, 첫 등장 iterator)
 	{
-		cout << *it << endl;
+		bool found = false;
+		vector<int>::iterator it;
+		// TODO
+		for (it = v.begin(); it != v.end(); ++it)
+		{
+			if ((*it) % 11 == 0)
+			{
+				found = true;
+				break;
+			}
+		}
 	}
-
-	multiset<int>::iterator itBegin2 = ms.lower_bound(100);
-	multiset<int>::iterator itEnd2 = ms.upper_bound(100);
-
-	for (multiset<int>::iterator it = itBegin2; it != itEnd2; ++it)
+	// Q3) 홀수인 숫자의 개수는? (count)
 	{
-		cout << *it << endl;
+		int count = 0;
+		int size = v.size();
+		for (int i = 0; i < size; i++)
+		{
+			if (v[i] % 2 == 1)
+				count++;
+		}
 	}
-
-
+	// Q4) 벡터에 들어가 있는 모든 숫자들에 3을 곱해주세요
+	{
+		int size = v.size();
+		for (int i = 0; i < size; i++)
+		{
+			v[i] *= 3;
+		}
+	}
 	return 0;
 }
