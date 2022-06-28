3e8565afaf93eff2c1869efe4a9cafb8b4157e72
cheonyongjun <yes1312@naver.com>
2022-06-28 22:28:39 +0900

    map #2

--- CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study.cpp
@@ -33,8 +33,11 @@ int main()
 		//int _key;
 		//Player* _value;
 	};
-
+	srand(static_cast<unsigned int>(time(nullptr)));
 	map<int, int> m;
+
+	m.insert(make_pair(1, 100));
+	m.insert(make_pair(1, 200)); // key 중복 불가
 
 	// 10만명 입장
 	for (int i = 0; i < 100000; i++)
@@ -54,7 +57,54 @@ int main()
 	// Q) ID = 1만인 Player를 찾고 싶다!
 	// A) 매우 빠르게 찾을 수 있음
 
-	m.find(10000);
+	map<int, int>::iterator findIt = m.find(10000);
+	if (findIt != m.end())
+	{
+		cout << "찾음" << endl;
+	}
+	else
+	{
+		cout << "못찾음" << endl;
+	}
+	
+	// map 순회
+	for (map<int, int>::iterator it = m.begin(); it != m.end(); ++it)
+	{
+		pair<const int, int>& p = (*it);
+		int key = p.first; // (*it).first it->first
+		int value = p.second;
 
+		cout << key << " " << value << endl;
+	}
+
+	// 없으면 추가, 있으면 수정
+	map<int, int>::iterator findIt = m.find(10000);
+	if (findIt != m.end())
+	{
+		findIt->second = 200;
+	}
+	else
+	{
+		m.insert(make_pair(10000, 200));
+	}
+
+	// 없으면 추가, 있으면 수정 v2
+	m[5] = 500; // 5라는 key가 있으면 500으로 수정, 없으면 (5, 500) 추가
+	
+	m.clear();
+
+	// [] 연산자 사용할 때 주의
+	// 대입을 하지 않더라도 (Key/Value) 형태의 데이터가 추가된다!
+	// 즉 없으면 기본값으로 세팅해줌
+	for (int i = 0; i < 10; i++)
+	{
+		cout << m[i] << endl;
+
+	}
+
+	// 넣고   (insert, [])
+	// 빼고   (erase)
+	// 찾고   (find, [])
+	// 반복자 (map::iterator) (*it) pair<key, value>&
 	return 0;
 }
