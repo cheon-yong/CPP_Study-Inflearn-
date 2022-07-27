b06bd2c283a37494128791686b37ff25325c6473
cheonyongjun <yes1312@naver.com>
2022-07-27 21:33:25 +0900

    using

--- CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study.cpp
@@ -1,80 +1,36 @@
 ﻿#include <iostream>
 using namespace std;
 #include <vector>
+#include <list>
 
-// 오늘의 주제 : nullptr
+// 오늘의 주제 : using
 
-void Test(int a)
-{
-	cout << "Test(int)" << endl;
-}
+typedef vector<int>::iterator VecIt;
 
-void Test(void* ptr)
-{
-	cout << "Test(*)" << endl;
-}
+typedef int id;
 
-class Knight
-{
-public:
-	void Test()
-	{
+using id2 = int;
 
-	}
-};
+// 1) 직관성
+typedef void (*MyFunc)();
+using MyFunc2 = void(*);
 
-Knight* FindKnight()
-{
-	return nullptr;
-}
 
-//NullPtr 구현
-const
-class
-{
-public:
-	// 그 어떤 타입의 포인터와도 치환 가능
-	template<typename T>
-	operator T* () const
-	{
-		return 0;
-	}
+// 2) 템플릿
+template<typename T>
+using List = std::list<T>;
 
-	template<typename C, typename T>
-	operator T C::* () const
-	{
-		return 0;
-	}
-private:
-	void operator&() const; // 주소값 &을 막는다
-} _nullptr;
 
 int main()
 {
-	int* ptr = NULL;// 0 NULL
+	id playerId = 0;
 
-	// 1) 오동작
-	Test(0);
-	//Test(NULL);
-	Test(_nullptr);
-
-	&_nullptr;
-	// 2) 가독성
-	Knight* knight = FindKnight();
-	if (knight == _nullptr)
-	{
-
-	}
-
-	void (Knight:: * memFunc)();
-	memFunc = &Knight::Test;
-
-	if (memFunc == _nullptr)
-	{
-
-	}
-
-	auto whoami = _nullptr;
+	List<int> li;
+	li.push_back(10);
+	li.push_back(10);
+	li.push_back(10);
+	li.push_back(10);
+	li.push_back(10);
 
 	return 0;
 }
