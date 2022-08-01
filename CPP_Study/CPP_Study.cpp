b6085e28eed2027e34ba0af6855d854d78b49d09
cheonyongjun <yes1312@naver.com>
2022-08-02 00:16:31 +0900

    smart pointer

--- CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study.cpp
@@ -5,148 +5,112 @@ using namespace std;
 #include <algorithm>
 #include <set>
 
-// 오늘의 주제 : 람다 (lamda)
+// 오늘의 주제 : 스마트 포인터 (smart pointer)
 
-// 함수 객체를 빠르게 만드는 문법
-enum class ItemType
-{
-	None,
-	Armor,
-	Weapon,
-	Jewelry,
-	Comsumable,
-};
-
-enum class Rarity
-{
-	Common,
-	Rare,
-	Unique,
-};
-
-class Item
+class Knight
 {
 public:
-	Item() { }
-	Item(int itemId, Rarity rarity, ItemType type)
-		: _itemId(itemId), _rarity(rarity), _type(type)
+	Knight() { cout << "Knight 생성" << endl; }
+	~Knight() { cout << "Knight 소멸" << endl; }
+	void Attack()
 	{
-
+		if (_target.expired() == false)
+		{
+			shared_ptr<Knight> sptr = _target.lock();
+			sptr->_hp -= _damage;
+			cout << "HP : " << sptr->_hp << endl;
+		}
 	}
 public:
-	int _itemId = 0;
-	Rarity _rarity = Rarity::Common;
-	ItemType _type = ItemType::None;
+	int _hp = 100;
+	int _damage = 10;
+	weak_ptr<Knight> _target;
 };
 
+class RefCountBlock
+{
+public:
+	int _refCount = 1;
+	int _weakCount = 1;
+};
+
+template<typename T>
+class SharedPtr
+{
+public:
+	SharedPtr() { }
+	SharedPtr(T* ptr) : _ptr(ptr)
+	{
+		if (_ptr != nullptr)
+		{
+			_block = new RefCountBlock();
+			cout << "RefCount : " << _block->_refCount << endl;
+		}
+	}
+	
+	SharedPtr(const SharedPtr& sptr) : _ptr(sptr._ptr), _block(sptr._block)
+	{
+		if (_ptr != nullptr)
+		{
+			_block->_refCount++;
+			cout << "RefCount : " << _block->_refCount << endl;
+		}
+	}
+
+	void operator=(const SharedPtr& sptr)
+	{
+		_ptr = sptr._ptr;
+		_block = sptr._block;
+
+		if (_ptr != nullptr)
+		{
+			_block->_refCount++;
+			cout << "RefCount : " << _block->_refCount << endl;
+		}
+	}
+
+	~SharedPtr()
+	{
+		if (_ptr != nullptr)
+		{
+			_block->_refCount--;
+			cout << "RefCount : " << _block->_refCount << endl;
+
+			if (_block->_refCount == 0)
+			{
+				delete _ptr;
+				//delete _block;
+				cout << "Delete Data" << endl;
+			}
+		}
+	}
+public:
+	T* _ptr = nullptr;
+	RefCountBlock* _block = nullptr;
+};
 int main()
 {
-	vector<Item> v;
+	/*Knight* k1 = new Knight();
+	Knight* k2 = new Knight();
 
-	v.push_back(Item(1, Rarity::Common, ItemType::Weapon));
-	v.push_back(Item(2, Rarity::Common, ItemType::Armor));
-	v.push_back(Item(3, Rarity::Rare, ItemType::Jewelry));
-	v.push_back(Item(4, Rarity::Unique, ItemType::Weapon));
+	k1->_target = k2;
+	k1->Attack();*/
 
-	// 람다 = 함수 객체를 손쉽게 만드는 문법
-	// 람다 자체로 C++11에 "새로운" 기능이 들어간 것은 아니다
+	// 스마트 포인터 : 포인터를 알맞는 정책에 따라 관리하는 객체 (포인터를 래핑해서 사용)
+	// shared_ptr, weak_ptr, unique_ptr
+
+	shared_ptr<Knight> k1 = make_shared<Knight>();
 	{
-		struct IsUniqueItem
-		{
-			bool operator()(Item& item)
-			{
-				return item._rarity == Rarity::Unique;
-			}
-		};
-
-		// 클로저 (closure) = 람다에 의해 만들어진 실행시점 객체
-		auto isUniqueLambda = [](Item& item)
-		{
-			return item._rarity == Rarity::Unique;
-		}; // 람다 표현식(lambda expression) 
-
-		auto findIt = std::find_if(v.begin(), v.end(),
-			[](Item& item) { return item._rarity == Rarity::Unique; });
-		if (findIt != v.end())
-			cout << "아이템 ID : " << findIt->_itemId << endl;
-
+		shared_ptr<Knight> k2 = make_shared<Knight>();
+		k1->_target = k2;
+		k2->_target = k1;
 	}
 
-	{
-		struct FindItem
-		{
-			FindItem(int itemId, Rarity rarity, ItemType type)
-				: _itemId(itemId), _rarity(rarity), _type(type)
-			{
+	k1->Attack();
 
-			}
-			bool operator()(Item& item)
-			{
-				return item._itemId == _itemId;
-			}
-
-			int _itemId;
-			Rarity _rarity;
-			ItemType _type;
-		};
-
-		int itemId = 4;
-		Rarity rarity = Rarity::Unique;
-		ItemType type = ItemType::Weapon;
-
-		// [ ] 캡처 (capture) : 함수 객체 내부에 변수를 저장하는 개념과 유사
-		// 사진을 찰각 [캡처]하듯... 일종의 스냅샷을 찍는다고 이해
-		// 기본 캡처 모드 : 값(복사) 방식(=) 참조 방식 (&)
-		// 변수마다 캡처모드를 지정해서 사용 가능 : 값 방식(name), 참조방식 (&name)
-		auto findByItem = [=](Item& item)
-		{
-			return item._itemId == itemId && item._rarity == rarity && type == item._type;
-		};
-
-
-		auto findIt = std::find_if(v.begin(), v.end(),
-			FindItem(4, Rarity::Unique, ItemType::Weapon));
-		if (findIt != v.end())
-			cout << "아이템 ID : " << findIt->_itemId << endl;
-
-
-	}
-	{
-		class Knight
-		{
-		public:
-			auto ResetHpJob()
-			{
-				auto f = [=]()
-				{
-					_hp = 200;
-				};
-
-				return f;
-			}
-
-		public:
-			int _hp = 100;
-		};
-
-		class Functor
-		{
-		public:
-			Functor(Knight* k) : _knight(k)
-			{
-
-			}
-			void operator()()
-			{
-				_knight->_hp = 200;
-			}
-		public:
-			Knight* _knight;
-		};
-
-		Knight* k = new Knight();
-		auto job = k->ResetHpJob();
-	}
+	// 복사하는 부분이 막혀있는 포인터
+	unique_ptr<Knight> uptr = make_unique<Knight>();
+	unique_ptr<Knight> uptr2 = std::move(uptr);
 
 	return 0;
 }
